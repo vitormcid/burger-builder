1157ac48a29aab31f7ddbe53385afb0c5b60ac32
Vitor <cid.vitor@hotmail.com>
2020-02-18 22:42:36 -0300

    add more features to burger builder

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,12 +1,18 @@
 import React from 'react';
+
 import classes from './BuildControl.css';
 
 const buildControl = (props) => (
-	<div ClassName={classes.BuildControl}>
-		<div className={classes.Label}>{props.label}</div>
-		<button className={classes.Less}>Less</button>
-		<button className={classes.More}>More</button>	
-	</div>
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button 
+            className={classes.Less} 
+            onClick={props.removed} 
+            disabled={props.disabled}>Less</button>
+        <button 
+            className={classes.More} 
+            onClick={props.added}>More</button>
+    </div>
 );
 
 export default buildControl;
